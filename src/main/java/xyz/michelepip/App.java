a533cf4769754ca5b77f2cac550754ee478e11f0
pipicm25 <michele@michelepip.xyz>
2023-09-28 18:55:58 +0930

    Initial

---
+++ src/main/java/xyz/michelepip/App.java
@@ -0,0 +1,22 @@
+package xyz.michelepip;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        this.getLogger().info("We're on");
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
