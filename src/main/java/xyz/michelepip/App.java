49fc3054e673b42b8fea66beec7b9363d390b978
pipicm25 <michele@michelepip.xyz>
2023-09-28 19:18:16 +0930

    Init. Working'

--- src/main/java/xyz/michelepip/App.java
+++ src/main/java/xyz/michelepip/App.java
@@ -1,11 +1,13 @@
 package xyz.michelepip;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
 /**
  * Hello world!
  *
  */
+@Plugin(name="guitest", version="1.0-SNAPSHOT")
 public class App extends JavaPlugin
 {
     @Override
