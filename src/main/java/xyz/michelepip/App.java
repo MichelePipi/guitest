e72acc9caec7a96f012680cce3c5a7324dcb7e5d
pipicm25 <michele@michelepip.xyz>
2023-09-28 21:15:39 +0930

    Add test cmd

--- src/main/java/xyz/michelepip/App.java
+++ src/main/java/xyz/michelepip/App.java
@@ -1,24 +1,30 @@
 package xyz.michelepip;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.command.Command;
+import org.bukkit.plugin.java.annotation.command.Commands;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
+
+import xyz.michelepip.cmd.HelloWorldCmd;
 
 /**
  * Hello world!
  *
  */
-@Plugin(name="guitest", version="1.0-SNAPSHOT")
-public class App extends JavaPlugin
-{
-    @Override
-    public void onEnable()
-    {
-        this.getLogger().info("We're on");
+@Plugin(name = "guitest", version = "1.0-SNAPSHOT")
+@Commands(@Command(name = "helloworld", permission = "guitest.helloworld", permissionMessage = "You do not have the required permissions to execute this command."))
+public class App extends JavaPlugin {
+    private void registerCommands() {
+        getCommand("helloworld").setExecutor(new HelloWorldCmd());
     }
 
     @Override
-    public void onDisable()
-    {
+    public void onEnable() {
+        registerCommands();
+    }
+
+    @Override
+    public void onDisable() {
 
     }
 }
---
+++ src/main/java/xyz/michelepip/cmd/HelloWorldCmd.java
@@ -0,0 +1,18 @@
+package xyz.michelepip.cmd;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+public class HelloWorldCmd implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(final CommandSender sender, final Command command,
+            final String label, final String[] args) {
+        final Player player = (Player) sender;
+        player.sendMessage("Hello, World.");
+        return true;
+    }
+}
