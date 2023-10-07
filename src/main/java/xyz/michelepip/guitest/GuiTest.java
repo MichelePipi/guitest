67f50aa83a1c32049f823108537fc91ee233316c
pipicm25 <michele@michelepip.xyz>
2023-10-07 18:52:46 +1030

    Switch to InventoryFramework

--- src/main/java/xyz/michelepip/guitest/GuiTest.java
+++ src/main/java/xyz/michelepip/guitest/GuiTest.java
@@ -6,14 +6,12 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import io.github.rysefoxx.inventory.plugin.pagination.InventoryManager;
 import xyz.michelepip.guitest.cmd.GuiTestCmd;
 import xyz.michelepip.guitest.cmd.HelloWorldCmd;
 import xyz.michelepip.guitest.cmd.moderation.warn.WarnCommand;
 import xyz.michelepip.guitest.events.EventTestHandler;
 import xyz.michelepip.guitest.events.filter.ChatFilterEventHandler;
 import xyz.michelepip.guitest.events.filter.cmd.RefreshChatFilterCommand;
-import xyz.michelepip.guitest.guitest.RyseInventoryTest;
 
 /**
  * Hello world!
@@ -26,7 +24,6 @@ public final class GuiTest extends JavaPlugin {
     private static Logger logger;
 
     private final PluginManager pluginManager = getServer().getPluginManager();
-    private final InventoryManager inventoryManager = new InventoryManager(this);
 
     @Override
     public void onLoad() {
@@ -45,7 +42,6 @@ public final class GuiTest extends JavaPlugin {
         saveDefaultConfig();
         reloadConfig();
         ChatFilterEventHandler.refreshFilter();
-        inventoryManager.invoke();
         infoLog("Enabled GuiTest Version " + getDescription().getVersion());
     }
 
@@ -59,7 +55,6 @@ public final class GuiTest extends JavaPlugin {
         getCommand("guitest").setExecutor(new GuiTestCmd());
         getCommand("refreshfilter").setExecutor(new RefreshChatFilterCommand());
         getCommand("warn").setExecutor(new WarnCommand());
-        getCommand("rysetest").setExecutor(new RyseInventoryTest());
     }
 
     private void registerEvents() {
